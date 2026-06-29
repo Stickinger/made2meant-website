90c253f113a62a50a499595820c3342640ffe8d8
Stickinger <office@made2meant.com>
2026-06-29 23:50:52 +0200

    Update supabase.js

--- Website/js/supabase.js
+++ Website/js/supabase.js
@@ -3,7 +3,7 @@
 // Hier trägst du deine eigenen Keys ein (nach Supabase-Projekt erstellen)
 // ============================================================
 
-const SUPABASE_URL = 'https://xqeednhajvftvcsjzfly.supabase.co';
+const SUPABASE_URL = 'https://xqeedhnajvftvcsjzfly.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhxZWVkaG5hanZmdHZjc2p6Zmx5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIzMzM4MTgsImV4cCI6MjA5NzkwOTgxOH0.Tdg6NCtiNmFwXzyRXaI5E7QX3dDdU4EToeclO0QqqTw';
 
 const { createClient } = supabase;
